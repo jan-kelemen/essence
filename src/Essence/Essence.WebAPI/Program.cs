c4ee578c3c3f4c6715263e2d62858b88cfb91624
Jan Kelemen <jkeleme@gmail.com>
2023-09-14 18:30:49 +0200

    Add blank template for WebAPI

---
+++ src/Essence/Essence.WebAPI/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace Essence.WebAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
