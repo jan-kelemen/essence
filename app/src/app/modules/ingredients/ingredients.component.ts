e6c73d44cb1f71ce8c7fd1e81d95babad50bf269
Jan Kelemen <jkeleme@gmail.com>
2024-02-18 20:40:57 +0100

    Add delete button for ingredients list

--- app/src/app/modules/ingredients/ingredients.component.ts
+++ app/src/app/modules/ingredients/ingredients.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { IngredientsService } from './ingredients.service';
 import { IngredientHeader } from './models/ingredient-header.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-ingredients',
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.css']
 })
-export class IngredientsComponent implements OnInit {
-  ingredients$: Observable<IngredientHeader[]>;
+export class IngredientsComponent {
+  list = [];
+  listSubject$ = new Subject<IngredientHeader[]>();
+  ingredients$ = this.listSubject$.asObservable();
 
   constructor(private ingredientsService: IngredientsService) {
     this.ingredients$ = ingredientsService.queryIngredients('');
   }
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  onDelete(id: string): void {
+    this.ingredientsService.deleteIngredient(id).subscribe(x => this.ingredients$ = this.ingredientsService.queryIngredients(''));
   }
 }
